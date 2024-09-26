3aebe7c087aa7ce590f5c5d541ff860492ef4b6a
ShyamalShah3 <shyamalgs@gmail.com>
2024-09-25 18:06:03 -0700

    Add LLama 3.2 models & support for cross region inference profiles

--- assets/layers/ai-hub-be/python/utils/enums.py
+++ assets/layers/ai-hub-be/python/utils/enums.py
@@ -33,13 +33,17 @@ class Provider(Enum):
     OPENAI = "openai"
 
 class BedrockModel(str,Enum):
-    CLAUDE_3_5_SONNET = "anthropic.claude-3-5-sonnet-20240620-v1:0"
-    # CLAUDE_3_OPUS = "anthropic.claude-3-opus-20240229-v1:0" # Issues with OPUS
-    CLAUDE_3_SONNET = "anthropic.claude-3-sonnet-20240229-v1:0"
-    CLAUDE_3_HAIKU = "anthropic.claude-3-haiku-20240307-v1:0"
+    CLAUDE_3_5_SONNET = "us.anthropic.claude-3-5-sonnet-20240620-v1:0"
+    CLAUDE_3_OPUS = "us.anthropic.claude-3-opus-20240229-v1:0" # Issues with OPUS
+    CLAUDE_3_SONNET = "us.anthropic.claude-3-sonnet-20240229-v1:0"
+    CLAUDE_3_HAIKU = "us.anthropic.claude-3-haiku-20240307-v1:0"
     LLAMA_3_1_8B_INSTRUCT = "meta.llama3-1-8b-instruct-v1:0"
     LLAMA_3_1_70B_INSTRUCT = "meta.llama3-1-70b-instruct-v1:0"
-    # LLAMA_3_1_405B_INSTRUCT = "meta.llama3-1-405b-instruct-v1:0" # Unauthorized
+    LLAMA_3_1_405B_INSTRUCT = "meta.llama3-1-405b-instruct-v1:0" # Unauthorized
+    LLAMA_3_2_1B_INSTRUCT = "us.meta.llama3-2-1b-instruct-v1:0"
+    LLAMA_3_2_3B_INSTRUCT = "us.meta.llama3-2-3b-instruct-v1:0"
+    LLAMA_3_2_11B_VISION_INSTRUCT = "us.meta.llama3-2-11b-instruct-v1:0"
+    LLAMA_3_2_90B_VISION_INSTRUCT = "us.meta.llama3-2-90b-instruct-v1:0"
     MISTRAL_7B_INSTRUCT = "mistral.mistral-7b-instruct-v0:2"
     MISTRAL_8_7B_INSTRUCT = "mistral.mixtral-8x7b-instruct-v0:1"
     MISTRAL_LARGE = "mistral.mistral-large-2402-v1:0"
