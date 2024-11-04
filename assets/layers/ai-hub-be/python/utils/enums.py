d4d624ccf5bcefb293d2987cb3f3acb78e1b538e
ShyamalShah3 <shyamalgs@gmail.com>
2024-11-04 14:13:09 -0800

    Add Claude 3.5 Haiku

--- assets/layers/ai-hub-be/python/utils/enums.py
+++ assets/layers/ai-hub-be/python/utils/enums.py
@@ -36,6 +36,7 @@ class Provider(Enum):
 class BedrockModel(str,Enum):
     CLAUDE_3_5_SONNET = "us.anthropic.claude-3-5-sonnet-20240620-v1:0"
     CLAUDE_3_5_SONNET_V2 = "us.anthropic.claude-3-5-sonnet-20241022-v2:0"
+    CLAUDE_3_5_HAIKU = "us.anthropic.claude-3-5-haiku-20241022-v1:0"
     CLAUDE_3_OPUS = "us.anthropic.claude-3-opus-20240229-v1:0" # Issues with OPUS
     CLAUDE_3_SONNET = "us.anthropic.claude-3-sonnet-20240229-v1:0"
     CLAUDE_3_HAIKU = "us.anthropic.claude-3-haiku-20240307-v1:0"
